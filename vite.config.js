48ed8620ec71dcc23be4c1cd97da73a44dab0ddb
Your Name <you@example.com>
2024-01-27 23:37:00 +0000

    aaaaaaaaaaa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+import { defineConfig } from 'vite'
+
+/*
 export default {
     root: 'src',
     build: {
         outDir: '../dist',
     },
 };
+*/
+
+export default defineConfig({
+    base: '/vite-practice/'
+})
