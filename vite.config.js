26e0a1f22386c2e2a29d17111975cc44f1defe57
Your Name <you@example.com>
2024-01-29 16:20:59 +0000

    lets see

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-// why isn't this thing working as intended,,,
+
 export default {
     root: 'src',
     build: {
         outDir: '../dist',
+        rollupOptions: {
+            input: {
+                home: 'src/index.html',
+                about: 'src/about.html',
+                'blog/how-to-flirt': 'src/blog/how-to-flirt.html'
+            }
+        }
     },
     base: '/vite-practice/',
 };
